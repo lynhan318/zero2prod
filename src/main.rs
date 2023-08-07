70e4d2221246815c4f4bbcc4c30af5569f3cbff2
Kevin <lythanhnhan27294@gmail.com>
2023-08-07 18:04:18 +0700

    feat: init

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use std::net::TcpListener;
+
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind address");
+    run(listener)?.await
+}
